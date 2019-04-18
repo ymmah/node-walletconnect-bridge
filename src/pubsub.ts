be56bd24ee6a09053b3c9da62528869466219b13
jpopxfile <raynold.sim@gmail.com>
2019-04-18 13:19:10 +0900

    Changed array to dictionary

--- src/pubsub.ts
+++ src/pubsub.ts
@@ -2,16 +2,33 @@ import WebSocket from 'ws'
 import { ISocketMessage, ISocketSub } from './types'
 import { pushNotification } from './notification'
 
-const subs: ISocketSub[] = []
-const pubs: ISocketMessage[] = []
 
-const setSub = (subscriber: ISocketSub) => subs.push(subscriber)
-const getSub = (topic: string) =>
-  subs.filter(subscriber => subscriber.topic === topic)
+const subs: Record<string, ISocketSub[]> = {}
+const pubs: Record<string, ISocketMessage[]> = {}
 
-const setPub = (socketMessage: ISocketMessage) => pubs.push(socketMessage)
-const getPub = (topic: string) =>
-  pubs.filter(pending => pending.topic === topic)
+const setSub = function (subscriber: ISocketSub, topic: string){ 
+  if (subs[topic] == null) {
+    subs[topic] = [subscriber]
+  } else {
+    subs[topic].push(subscriber)
+  }
+}
+
+const getSub = function (topic: string): ISocketSub[] { 
+  return subs[topic]
+}
+
+const setPub = function (socketMessage: ISocketMessage, topic: string){ 
+  if (pubs[topic] == null) {
+    pubs[topic] = [socketMessage]
+  } else {
+    pubs[topic].push(socketMessage)
+  } 
+}
+
+const getPub = function (topic: string): ISocketMessage[] { 
+  return pubs[topic]
+}
 
 function socketSend (socket: WebSocket, socketMessage: ISocketMessage) {
   if (socket.readyState === 1) {
@@ -25,7 +42,7 @@ const SubController = (socket: WebSocket, socketMessage: ISocketMessage) => {
 
   const subscriber = { topic, socket }
 
-  setSub(subscriber)
+  setSub(subscriber, topic)
 
   const pending = getPub(topic)
 
@@ -42,12 +59,12 @@ const PubController = (socketMessage: ISocketMessage) => {
   // send push notifications
   pushNotification(socketMessage.topic)
 
-  if (subscribers.length) {
+  if (subscribers != null) {
     subscribers.forEach((subscriber: ISocketSub) =>
       socketSend(subscriber.socket, socketMessage)
     )
   } else {
-    setPub(socketMessage)
+    setPub(socketMessage, socketMessage.topic)
   }
 }
 
